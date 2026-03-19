b107cf6c3a4ed02f7613f3e5332119d8dcf62f5b
Beni K <beni.kinczel@gmail.com>
2026-03-20 00:14:32 +1100

    docs: updated version

--- internal/server/server.go
+++ internal/server/server.go
@@ -32,7 +32,7 @@ import (
 	"github.com/echo-ssr/echo/internal/watcher"
 )
 
-const version = "1.0.0"
+const version = "1.0.1"
 
 // ---------------------------------------------------------------------------
 // Types
